4546454a15498297ef0201e25a524b1b9a4bc223
Maria Guadalupe Clemente Tellez <240613@utxicotepec.edu.mx>
2026-01-08 14:28:58 -0600

    Crear funciones JS y estilo del encabezado

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,3 +1,31 @@
 function saludar() {
     alert("Bienvenido a la Academia de T.I");
 }
+
+function suma() {
+    var valor1 =24;
+    var valor2=13;
+    var resultado=valor1+valor2;
+    alert("Resultado de la suma de "+valor1+ " mas " + valor2 + " es: " + resultado);
+}
+
+function resta() {
+    var valor1 =50;
+    var valor2=25;
+    var resultado=valor1-valor2;
+    alert("Resultado de la resta de "+valor1+ " menos " + valor2 + " es: " + resultado);
+}
+
+function multiplicacion() {
+    var valor1 =10;
+    var valor2=10;
+    var resultado=valor1*valor2;
+     alert("Resultado de la multiplicacion de "+valor1+ " por " + valor2 + " es: " + resultado);
+}
+
+function division() {
+    var valor1 =100;
+    var valor2=4;
+    var resultado=valor1/valor2;
+     alert("Resultado de la division de "+valor1+ " entre " + valor2 + " es: " + resultado);
+}
